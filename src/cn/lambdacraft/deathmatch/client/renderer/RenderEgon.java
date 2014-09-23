f4fae86778e7811f7a8d141f8eb9800a1c3a4a61
WeAthFoLD <Cheetah77@qq.com>
2014-09-23 15:44:57 +0800

    Parallel update with Weaponry

---
+++ src/cn/lambdacraft/deathmatch/client/renderer/RenderEgon.java
@@ -0,0 +1,48 @@
+package cn.lambdacraft.deathmatch.client.renderer;
+
+import net.minecraft.client.renderer.RenderBlocks;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+
+import org.lwjgl.opengl.GL11;
+
+import cn.lambdacraft.core.prop.ClientProps;
+import cn.lambdacraft.deathmatch.client.model.ModelEgonHead;
+import cn.liutils.api.client.render.RenderModelItem;
+import cn.weaponmod.api.client.render.RendererBulletWeapon;
+import cn.weaponmod.core.event.ItemControlHandler;
+
+/**
+ * Weapon Egon Renderer Class.
+ */
+public class RenderEgon extends RenderModelItem {
+
+	public RenderEgon() {
+		super(new ModelEgonHead(), ClientProps.EGON_HEAD_PATH);
+		this.renderInventory = false;
+		this.setStdRotation(5.0F, 173.42F, 0.0F);
+		this.inventorySpin = false;
+		this.setEquipOffset(0.318, -0.142, -0.142);
+		this.setOffset(0.0F, 0.2F, -0.302F);
+		this.setScale(2.46F);
+	}
+
+	Tessellator t = Tessellator.instance;
+
+	@Override
+	public void renderEquipped(ItemStack item, RenderBlocks render,
+			EntityLivingBase entity, ItemRenderType type) {
+		super.renderEquipped(item, render, entity, type);
+		GL11.glColor4f(1.0F, 1.0F, 1.0F, 0.5F);
+		if(entity instanceof EntityPlayer) {
+			EntityPlayer player = (EntityPlayer) entity;
+			if(player.getCurrentEquippedItem() == item &&
+					ItemControlHandler.getUsingTicks(player, 0) > 0) {
+				RendererBulletWeapon.renderMuzzleflashIn2d(Tessellator.instance, ClientProps.EGON_MUZZLE, 0.0F, -0.12F, -0.2F);
+			}
+		}
+	}
+	
+}
---
+++ src/cn/lambdacraft/deathmatch/item/weapon/Weapon_Crossbow.java
@@ -0,0 +1,170 @@
+package cn.lambdacraft.deathmatch.item.weapon;
+
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.IIcon;
+import net.minecraft.world.World;
+import cn.lambdacraft.api.hud.ISpecialCrosshair;
+import cn.lambdacraft.core.CBCMod;
+import cn.lambdacraft.crafting.register.CBCItems;
+import cn.lambdacraft.deathmatch.entity.EntityCrossbowArrow;
+import cn.liutils.api.entity.EntityBullet;
+import cn.weaponmod.api.WMInformation;
+import cn.weaponmod.api.action.Action;
+import cn.weaponmod.api.action.ActionReload;
+import cn.weaponmod.api.action.ActionShoot;
+import cn.weaponmod.api.feature.IModdable;
+import cn.weaponmod.api.information.InfUtils;
+import cn.weaponmod.api.information.InfWeapon;
+import cn.weaponmod.api.weapon.IZoomable;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+/**
+ * Crossbow in HLDM Style. Mode I : Non-delay, sniper rifle style. Mode II :
+ * Explosive arrow.
+ * 
+ * @author WeAthFolD
+ */
+public class Weapon_Crossbow extends WeaponGeneralBullet_LC implements
+		IModdable, IZoomable, ISpecialCrosshair {
+	
+	public class ActionLeft extends ActionShoot {
+
+		public ActionLeft() {
+			super(0, 0, "lambdacraft:weapons.xbow_fire");
+			setShootRate(30);
+		}
+		
+		protected Entity getProjectileEntity(World world, EntityPlayer player) {
+			return new EntityCrossbowArrow(world, player, getMode(player.getCurrentEquippedItem()) == 0);
+		}
+		
+	}
+
+	public IIcon[] sideIcons = new IIcon[6];
+
+	public Weapon_Crossbow() {
+		super(CBCItems.ammo_bow);
+
+		setUnlocalizedName("weapon_crossbow");
+		setCreativeTab(CBCMod.cct);
+		setMaxStackSize(1);
+		setMaxDamage(6);
+		setNoRepair();
+		iconName = "weapon_crossbow";
+		
+		actionShoot = new ActionLeft();
+		actionReload = new ActionReload(55, "", "");
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerIcons(IIconRegister reg) {
+		super.registerIcons(reg);
+		for (int i = 0; i < 6; i++) {
+			sideIcons[i] = reg.registerIcon("lambdacraft:crossbow_side" + i);
+		}
+	}
+
+	@Override
+	public void onUpdate(ItemStack is, World par2World,
+			Entity entity, int par4, boolean par5) {
+		super.onWpnUpdate(is, par2World, entity, par4, par5);
+		if(!par5) {
+			NBTTagCompound nbt = loadCompound(is);
+			if(nbt.getInteger("mode") == 1)
+				nbt.setInteger("mode", 0);
+		}
+		// TODO:Left to be finished
+		/*
+		if (entity instanceof EntityPlayer) {
+			EntityPlayer player = (EntityPlayer) entity;
+			int mode = getMode(par1ItemStack);
+			if (par5) {
+				if (mode == 1)
+					player.capabilities.setPlayerWalkSpeed(0.005F);
+				else
+					player.capabilities.setPlayerWalkSpeed(0.1F);
+
+			} else {
+				ItemStack stack = player.getCurrentEquippedItem();
+				if (stack == null || stack.itemID != itemID) 
+					player.capabilities.setPlayerWalkSpeed(0.1F);
+			}
+		}
+		
+		
+		if (!par5) {
+			NBTTagCompound nbt = loadCompound(par1ItemStack);
+			nbt.setInteger("mode", 0);
+		}
+		 */
+	}
+
+	@Override
+	public void onPlayerStoppedUsing(ItemStack par1ItemStack, World par2World,
+			EntityPlayer par3EntityPlayer, int par4) {
+		super.onPlayerStoppedUsing(par1ItemStack, par2World, par3EntityPlayer,
+				par4);
+	}
+	
+	public static boolean isBowPulling(ItemStack item) {
+		InfWeapon information = WMInformation.getInformation(item, true);
+		if (information == null)
+			return false;
+		return !(InfUtils.getDeltaTick(information, "shoot") < 17);
+	}
+
+	@Override
+	public void onModeChange(ItemStack item, EntityPlayer player, int newMode) {
+		NBTTagCompound nbt = loadCompound(item);
+		nbt.setInteger("mode", newMode);
+	}
+
+	@Override
+	public int getMode(ItemStack item) {
+		return loadCompound(item).getInteger("mode");
+	}
+
+	@Override
+	public int getMaxModes() {
+		return 2;
+	}
+
+	@Override
+	public String getModeDescription(int mode) {
+		return null;
+	}
+
+	private NBTTagCompound loadCompound(ItemStack itemStack) {
+		if (itemStack.stackTagCompound == null)
+			itemStack.stackTagCompound = new NBTTagCompound();
+		return itemStack.stackTagCompound;
+	}
+
+	@Override
+	public boolean isItemZooming(ItemStack stack, World world,
+			EntityPlayer player) {
+		return getMode(stack) == 1;
+	}
+
+	@Override
+	public int getHalfWidth() {
+		return 12;
+	}
+
+	@Override
+	public int getCrosshairID(ItemStack is) {
+		return getMode(is) == 1 ? 15 : 0;
+	}
+
+	@Override
+	public boolean doesSlowdown(ItemStack stack, World world,
+			EntityPlayer player) {
+		return false;
+	}
+}
